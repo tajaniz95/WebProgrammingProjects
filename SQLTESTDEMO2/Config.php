0a30d349c871e44c43f9ee8636bc53c6564b7bbd
tajaniz95 <zaheertajani95@gmail.com>
2017-11-16 16:20:10 -0500

    more tings

--- SQLTESTDEMO2/Config.php
+++ SQLTESTDEMO2/Config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'user123');
-   define('DB_PASSWORD', 'user123');
-   define('DB_DATABASE', 'user123');
+   define('DB_USERNAME', 'ztajani1');
+   define('DB_PASSWORD', 'ztajani1');
+   define('DB_DATABASE', 'ztajani1');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
